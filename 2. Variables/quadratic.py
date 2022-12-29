3519d32520c62b133b358eed070dd7f8b49d12f9
Terézia Turyová <terezia.turyova@student.tuke.sk>
2022-12-29 14:42:38 +0100

    adding quadratic.py

---
+++ 2. Variables/quadratic.py
@@ -0,0 +1,20 @@
+# 9. Quadratic
+
+# input -> default is string
+username = input("Enter your name: ")
+print(username)
+
+# input for int
+age = int(input("Enter your age: ")) # only integer is accepted, else error is displayed
+print(age)
+
+# task
+print("Quadratic formula calculator")
+a = int(input("Enter value for a (int): "))
+b = int(input("Enter value for b (int): "))
+c = int(input("Enter value for c (int): "))
+
+root1 = (-b + ((b ** 2 - (4 * a * c)) ** 0.5)) / 2 * a
+root2 = (-b - ((b ** 2 - (4 * a * c)) ** 0.5)) / 2 * a
+print(root1)
+print(root2)
