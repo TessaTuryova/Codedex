00732a2b1694d789be8af8c50646b32c3a3f548b
Terézia Turyová <terezia.turyova@student.tuke.sk>
2022-12-29 14:53:43 +0100

    adding coin_flip.py

---
+++ 3. Control Flow/coin_flip.py
@@ -0,0 +1,10 @@
+# 11. Coin Flip
+
+import random
+
+num = random.randint(0, 1)   # RNGesus will give us a random number that is either 0 or 1
+
+if num > 0.5:
+    print('Heads')
+else:
+    print('Tails')
