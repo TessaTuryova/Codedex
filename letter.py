ae4db9074d63c787aad5b3e434e99f4c8814a41e
Terézia Turyová <terezia.turyova@student.tuke.sk>
2022-12-29 13:45:36 +0100

    adding letter.py

---
+++ letter.py
@@ -0,0 +1,5 @@
+print("today's date is 29th of December 2022")
+print("I've decided once again to pick up learning Python in my free time and I'm excited for it")
+print("in the codedex.io course I hope to refresh the basics so I can build on this knowledge further")
+print("a note for my future self: hope you're better off then now lul, smarter and not so anxious")
+print("aye that's all")
