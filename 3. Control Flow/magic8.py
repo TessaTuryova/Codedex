bd6dca34364348d01bf837d16fe58b24836e2c6a
Terézia Turyová <terezia.turyova@student.tuke.sk>
2022-12-30 20:49:02 +0100

    editing magic8.py

--- 3. Control Flow/magic8.py
+++ 3. Control Flow/magic8.py
@@ -2,26 +2,30 @@
 
 import random
 
-num = random.randint(0, 8) # generates rundom num from 0 to 8
+num = random.randint(0, 8)  # generates rundom num from 0 to 8
 print(num)
 
-print("Question: Am I old?")
+question = input('Question: ')
+# print("Question: Am I old?")
 
 if num == 0:
-    print("Yes - definitely.")
+    answer = "Yes - definitely."
 elif num == 1:
-    print("It is decidedly so.")
+    answer = "It is decidedly so."
 elif num == 2:
-    print("Without a doubt.")
+    answer = "Without a doubt."
 elif num == 3:
-    print("Reply hazy, try again.")
+    answer = "Reply hazy, try again."
 elif num == 4:
-    print("Ask again later.")
+    answer = "Ask again later."
 elif num == 5:
-    print("Better not tell you now.")
+    answer = "Better not tell you now."
 elif num == 6:
-    print("My sources say no.")
+    answer = "My sources say no."
 elif num == 7:
-    print("Outlook not so good.")
+    answer = "Outlook not so good."
 else:
-    print("Very doubtful.")
+    answer = "Very doubtful."
+
+print('Question:      ' + question)
+print('Magic 8 Ball:  ' + answer)
