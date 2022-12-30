be80e9245812a6c6140fb22296965bf4e416f6da
Terézia Turyová <terezia.turyova@student.tuke.sk>
2022-12-30 20:34:40 +0100

    adding grades.py

---
+++ 3. Control Flow/grades.py
@@ -0,0 +1,16 @@
+# 12. Grades
+
+#if condition:
+    # code inside
+
+grade = 56
+
+if grade > 60:
+    print("You passed!")
+else:
+    print("You failed.")
+
+if grade > 55:
+    print("You passed!")
+else:
+    print("You failed.")
