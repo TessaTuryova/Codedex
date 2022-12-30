a8c4a693e547a4a0ef5b9ba1b2099a9b6713a5db
Terézia Turyová <terezia.turyova@student.tuke.sk>
2022-12-30 20:38:42 +0100

    adding ph_levels.py

---
+++ 3. Control Flow/ph_levels.py
@@ -0,0 +1,30 @@
+# 13. pH Levels
+
+# == equal to
+# != not equal to
+# > greater than
+# < less than
+# >= greater than or equal to
+# <= less than or equal to
+
+grade = 0
+
+if grade > 90:
+  print('A')
+elif grade > 80:
+  print('B')
+elif grade > 70:
+  print('C')
+elif grade > 60:
+  print('D')
+else:
+  print('F')
+
+pH = 7
+
+if pH > 7:
+    print("Basic")
+elif pH < 7:
+    print("Acidic")
+else:
+    print("Neutral")
