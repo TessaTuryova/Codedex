57f1b4d28df62ee3d811ca8392a1b92bca3878a7
Terézia Turyová <terezia.turyova@student.tuke.sk>
2022-12-31 18:39:55 +0100

    adding enter_pin.py

---
+++ 4. Loops/enter_pin.py
@@ -0,0 +1,11 @@
+# 16. Enter PIN
+
+print("BANK OF CODEDEX")
+
+pin = int(input("Enter your PIN: "))
+
+while pin != 1234:
+    pin = int(input("Incorrect PIN. Try again: "))
+
+    if pin == 1234:
+        print("PIN accepred.")
