637f5101b138b3b1a57f611a0ab4646d73c5d5f5
Terézia Turyová <terezia.turyova@student.tuke.sk>
2023-01-01 06:19:59 +0100

    adding reading.py

---
+++ 5. Lists/reading.py
@@ -0,0 +1,18 @@
+# 24. Reading
+
+books = [
+    "Zero to One",
+    "The Lean Startup",
+    "The Mom Test",
+    "Made To Stick",
+    "Life In Code"
+]
+
+print(books)
+
+books.append("Zero to Sold") # adds string to last index of an array
+books.remove("Zero to One") # removes specified item
+#books.remove("e") ValueError: list.remove(x): x not in list
+books.pop(0) # removes first item
+
+print(books)
