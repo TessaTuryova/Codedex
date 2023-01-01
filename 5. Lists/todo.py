ff0d437e6ca32cf947ebe500f4b7bbca85da0049
Terézia Turyová <terezia.turyova@student.tuke.sk>
2023-01-01 06:14:44 +0100

    adding todo.py

---
+++ 5. Lists/todo.py
@@ -0,0 +1,17 @@
+# 22. To-Do
+
+todo = [
+    "Get quarters",
+    "Do laundry",
+    "Go for a walk",
+    "Get a haircut",
+    "Make some tea",
+    "Complete lists chapter",
+    "Call mom",
+    "Watch My Hero Academia"
+]
+
+print(todo[0]) # Get quarters
+print(todo[1]) # Do laundry
+print(todo[2 : 5]) # ['Go for a walk', 'Get a haircut', 'Make some tea']
+print(todo[9]) # gets out of range error: IndexError: list index out of range
