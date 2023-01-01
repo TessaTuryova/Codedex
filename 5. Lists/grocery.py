a8769ae0c1487d0df55648a264c5a9d66cdab10c
Terézia Turyová <terezia.turyova@student.tuke.sk>
2023-01-01 06:10:59 +0100

    adding grocery.py

---
+++ 5. Lists/grocery.py
@@ -0,0 +1,14 @@
+# 21. Grocery
+
+grocery = [
+    "Eggs",
+    "Avocados",
+    "Cookies",
+    "Hot pepper Jam",
+    "Blueberries",
+    "Broccoli"
+]
+
+print(grocery)
+
+# output: ['Eggs', 'Avocados', 'Cookies', 'Hot pepper Jam', 'Blueberries', 'Broccoli']
