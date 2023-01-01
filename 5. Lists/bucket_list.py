cc0a8f6e4ac521cb6472147b1e55026578f3d179
Terézia Turyová <terezia.turyova@student.tuke.sk>
2023-01-01 06:25:20 +0100

    adding bucket_list.py

---
+++ 5. Lists/bucket_list.py
@@ -0,0 +1,13 @@
+# 26 Bucket List
+
+things_to_do = [
+    "Build a meaningful product for everyone.",
+    "Try out hiking and mountain biking.",
+    "Visit at least 10 countries in my lifetime.",
+    "Produce an original song",
+    "Write a short story",
+    "Finish a 10k marathon."
+]
+
+for thing in things_to_do:
+    print(thing)
